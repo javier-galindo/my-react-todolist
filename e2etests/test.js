ce989e047463f5cbb33e9b1c729df0651ec950bc
Javier Galindo <javigalindo.sopra@gmail.com>
2018-04-10 13:49:35 +0200

    Part 2 - Add Todo

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,3 +1,4 @@
+/* global describe, it, browser */
 const expect = require('chai').expect;
 
 describe('TodoList App', () => {
@@ -7,4 +8,14 @@ describe('TodoList App', () => {
 
     expect(actualTitle).to.eql('Todo List');
   });
+
+  it('Should allow me to create a Todo', () => {
+    const todoText = 'Get better at testing';
+    browser.url('http://localhost:3000/');
+    browser.element('.todo-input').setValue(todoText);
+    browser.click('.todo-submit');
+    const actual = browser.element('.todo-text').getText();
+
+    expect(actual).to.equal(todoText);
+});
 });
